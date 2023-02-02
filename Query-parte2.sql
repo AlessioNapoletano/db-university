8c57cb2762be9e4e7a1d262d47d6d18834956c03
AlessioNapoletano <alessio.napoletano.job@gmail.com>
2023-02-02 15:29:39 +0100

    Ex on join, Upload es 1

--- Query-parte2.sql
+++ Query-parte2.sql
@@ -26,7 +26,15 @@
 
 --> Consegna JOIN
 1. Selezionare tutti gli studenti iscritti al Corso di Laurea in Economia
+    SELECT * 
+    FROM `degrees` 
+    JOIN `students` 
+    ON `students`.`degree_id` = `degrees`.`id` 
+    WHERE `degrees`.`name` = 'Corso di Laurea in economia'; 
+-------------------------------------------------------------------------------------------------
+
 2. Selezionare tutti i Corsi di Laurea Magistrale del Dipartimento di Neuroscienze
+
 3. Selezionare tutti i corsi in cui insegna Fulvio Amato (id=44)
 4. Selezionare tutti gli studenti con i dati relativi al corso di laurea a cui sono iscritti e il
 relativo dipartimento, in ordine alfabetico per cognome e nome
