433ad483edd8f0199fc8763210a40aa2b2328ff1
AlessioNapoletano <alessio.napoletano.job@gmail.com>
2023-02-02 16:05:24 +0100

    Upload Es 3

--- Query-parte2.sql
+++ Query-parte2.sql
@@ -41,7 +41,19 @@
     WHERE `degrees`.`level` = 'magistrale' 
     AND `departments`.`name` = 'Dipartimento di Neuroscienze'; 
 ---------------------------------------------------------------------------------------------------
+
 3. Selezionare tutti i corsi in cui insegna Fulvio Amato (id=44)
+    SELECT `degrees`.*
+    FROM `teachers`
+    JOIN `course_teacher`
+    ON `course_teacher`.`teacher_id` = `teachers`.`id`
+    JOIN `courses`
+    ON `courses`.`id` = `course_teacher`.`course_id`
+    JOIN `degrees`
+    ON `degrees`.`id` = `courses`.`degree_id`
+    WHERE `teachers`.`id` = 44;
+--------------------------------------------------------------------------------------------------
+
 4. Selezionare tutti gli studenti con i dati relativi al corso di laurea a cui sono iscritti e il
 relativo dipartimento, in ordine alfabetico per cognome e nome
 5. Selezionare tutti i corsi di laurea con i relativi corsi e insegnanti
